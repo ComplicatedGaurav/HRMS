040fc8edf4067f7a55cc52f62efb2d8b9029e41e
Gaurav Deshmukh <gauravpd1211@gmail.com>
2024-10-19 14:25:36 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.static('Public'))
 app.use(cors({
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true,
-    origin: "http://localhost:5173", // Update with your frontend 
+    origin: "https://hrms-xg6q.vercel.app", // Update with your frontend 
 }));
 
 app.use('/', require('./routes/authRoutes'))
